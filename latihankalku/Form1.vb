e5f6b729e19812249a15320455ea2d2182160107
Arza Fawazi <arza.fawazi103@gmail.com>
2024-08-21 21:27:56 +0200

    first commit

---
+++ latihankalku/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Dim op As String
+    Dim num1 As Decimal
+    Dim num2 As Decimal
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text & 1
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text & 2
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        TextBox1.Text = TextBox1.Text & 3
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        TextBox1.Text = TextBox1.Text & 4
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        TextBox1.Text = TextBox1.Text & 5
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text & 6
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text & 7
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        TextBox1.Text = TextBox1.Text & 8
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        TextBox1.Text = TextBox1.Text & 9
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        TextBox1.Text = TextBox1.Text & 0
+    End Sub
+
+    Private Sub Button12_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button12.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button14_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button14.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = ""
+        op = "+"
+    End Sub
+
+    Private Sub Button13_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button13.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = ""
+        op = "*"
+    End Sub
+
+    Private Sub Button15_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button15.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = ""
+        op = "-"
+    End Sub
+
+    Private Sub Button16_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button16.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = ""
+        op = "/"
+    End Sub
+
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        num2 = TextBox1.Text
+        If op = "+" Then
+            TextBox1.Text = num1 + num2
+        ElseIf op = "-" Then
+            TextBox1.Text = num1 - num2
+        ElseIf op = "*" Then
+            TextBox1.Text = num1 * num2
+            ElseIf = "/" Then
+            TextBox1.Text = num1 / num2
+        End If
+
+    End Sub
+End Class
